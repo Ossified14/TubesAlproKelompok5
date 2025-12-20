d3441c068dd616e625512a56a10a87b4f65e8725
Munif <munif.syahli@gmail.com>
2025-12-20 13:35:28 +0700

    menmbahkan kerangka logik untuk fungsi taampilkan pesanan

--- src/metode/TampilkanPesanan.java
+++ src/metode/TampilkanPesanan.java
@@ -3,11 +3,26 @@ package metode;
 import data.Pesanan;
 
 public class TampilkanPesanan {
-  private Pesanan pesanan = new Pesanan();
+
+  private static Pesanan pesanan = new Pesanan();
   
-  public void TampilkanPesanan(){
-    for (String x : pesanan.dataPesanan) {
-      System.out.println(x);
+  public static  void TampilkanPesanan(){
+    for (int i = 0; i < pesanan.dataPesanan.size(); i++) {
+      System.out.println("nama: "+pesanan.dataPesanan.get(i));
+      System.out.println("jumlah: "+pesanan.dataTotalLembar.get(i));
+      System.out.println("harga1an: "+pesanan.dataHarga.get(i));
     }
+  }
+
+  public static void main(String[] args) {
+    pesanan.dataPesanan.add("Pulpen");
+    pesanan.dataHarga.add(5000);
+    pesanan.dataTotalLembar.add(1);
+    pesanan.dataPesanan.add("pensil");
+    pesanan.dataHarga.add(3500);
+    pesanan.dataTotalLembar.add(1);
+
+
+    TampilkanPesanan();
   }
 }
