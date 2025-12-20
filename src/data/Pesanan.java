6ffb5a804db189075c23c08572b550929a981b0c
Sadid2006-ai <honkaisadid@gmail.com>
2025-12-20 12:39:04 +0700

    mengedit pesanan dan fungsi TambagJenisLayanan

--- src/data/Pesanan.java
+++ src/data/Pesanan.java
@@ -4,5 +4,7 @@ import java.util.ArrayList;
 
 public class Pesanan {
   public ArrayList<String> dataPesanan = new ArrayList<String>();
-  public ArrayList<Integer> dataKuantitasPesanan = new ArrayList<Integer>();
+  public ArrayList<Integer> dataHarga = new ArrayList<Integer>();
+  public ArrayList<Integer> dataTotalLembar = new ArrayList<Integer>();
+  public int totalBiaya;
 }
--- src/metode/TambahJenisLayanan.java
+++ src/metode/TambahJenisLayanan.java
@@ -1,17 +1,48 @@
 package metode;
 
+import data.JenisPesanan;
 import data.Pesanan;
 import java.util.Scanner;
 
 public class TambahJenisLayanan {
   private static Scanner input = new Scanner(System.in);
 
-  Pesanan pesanan = new Pesanan();
+  private static Pesanan pesanan = new Pesanan();
+  private static JenisPesanan jenisPesanan= new JenisPesanan();
 
-  private void tambahPesanan(String e) {
-    System.out.print("input pesanan:");
-    String x = input.nextLine();
+  private void tambahPesanan() {
+    System.out.print("Masukkan angka dari Jenis Layanan Yang Diinginkan (Print(1)/Fotocopy(2)): ");
+    int Jenis = input.nextInt();
 
-    pesanan.dataPesanan.add(x);
+    System.out.print("Masukkan Jumlah Lembar: ");
+    int Jumlah = input.nextInt();
+
+    if(Jenis == 1){
+      pesanan.dataPesanan.add(jenisPesanan.dataJenisPesanan[0]);
+      pesanan.dataHarga.add(jenisPesanan.hargaDataJenisPesanan[0]);
+      pesanan.dataTotalLembar.add(Jumlah);
+    }else if(Jenis == 2){
+      pesanan.dataPesanan.add(jenisPesanan.dataJenisPesanan[1]);
+      pesanan.dataHarga.add(jenisPesanan.hargaDataJenisPesanan[1]);
+      pesanan.dataTotalLembar.add(Jumlah);
+    }else{
+      System.out.println("Jenis Layanan Tidak tersedia");
+    }
   }
+
+  // public static void main(String[] args) {
+  //   TambahJenisLayanan test = new TambahJenisLayanan();
+
+  //   test.tambahPesanan();
+    
+  //   for (String string : pesanan.dataPesanan) {
+  //     System.out.println(string);
+  //   }
+  //   for (int harga : pesanan.dataHarga) {
+  //     System.out.println(harga);
+  //   }
+  //   for (int kuantitas : pesanan.dataTotalLembar) {
+  //     System.out.println(kuantitas);
+  //   }
+  // }
 }
