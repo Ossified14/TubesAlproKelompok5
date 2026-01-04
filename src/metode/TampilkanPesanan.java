c1b8d01241a5f3dda5c58df76e9e1711bb54d257
Munif <munif.syahli@gmail.com>
2026-01-04 19:37:06 +0700

    memperbaiki function di TampilkanPesanan

--- src/metode/TampilkanPesanan.java
+++ src/metode/TampilkanPesanan.java
@@ -6,23 +6,17 @@ public class TampilkanPesanan {
 
   private static Pesanan pesanan = new Pesanan();
 
-  public static void TampilkanPesanan(int index) {
-    if (index < pesanan.dataPesanan.size()){
-      System.out.println("===PESANAN KE -" + (index + 1) + "===");
-      System.out.println("Nama :" + pesanan.dataPesanan.get(index));
-      System.out.println("Jumlah :" + pesanan.dataTotalLembar.get(index));
-      System.out.println("Harga per lembar : Rp" + pesanan.dataHarga.get(index));
-      System.out.println("-----------------------");
-    }
-  }
   public static void TampilkanPesanan(){
     if (pesanan.dataPesanan.isEmpty()) {
       System.out.println("Belum ada data pesanan.");
       return;
     }
-    for(int i= 0;i < pesanan.dataPesanan.size(); i++){
-      TampilkanPesanan(i);
+    for(int i= 0 ;i < pesanan.dataPesanan.size(); i++){
+      System.out.println("===PESANAN KE -" + (i + 1) + "===");
+      System.out.println("Nama :" + pesanan.dataPesanan.get(i));
+      System.out.println("Jumlah :" + pesanan.dataTotalLembar.get(i));
+      System.out.println("Harga per lembar : Rp" + pesanan.dataHarga.get(i));
+      System.out.println("-----------------------");
     }
   }
 }
-
