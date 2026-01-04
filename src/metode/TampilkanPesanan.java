a24b5ff3324ebe642d594e1bcfe0db1af99970c8
hilyaaaaa <hanihilyakayla@gmail.com>
2026-01-04 11:00:27 +0700

    update

--- src/metode/TampilkanPesanan.java
+++ src/metode/TampilkanPesanan.java
@@ -4,45 +4,36 @@ import data.Pesanan;
 
 public class TampilkanPesanan {
 
- public static void tampilkanPesanan(int index) {
-        if (index < pesanan.dataPesanan.size()) {
-            System.out.println("=== PESANAN KE-" + (index + 1) + " ===");
-            System.out.println("Nama: " + Pesanan.dataPesanan.get(index));
-            System.out.println("Jumlah: " + Pesanan.dataTotalLembar.get(index));
-            System.out.println("Harga per lembar: Rp " + Pesanan.dataHarga.get(index));
-            System.out.println("------------------------");
-  private static Pesanan Pesanan = new Pesanan();
-        }
+  private static Pesanan pesanan = new Pesanan();
 
-         public static void TampilkanPesanan() {
-        if (pesanan.dataPesanan.isEmpty()) {
-            System.out.println("Belum ada data pesanan.");
-            return;
-        }
-        
-        for (int i = 0; i < pesanan.dataPesanan.size(); i++) {
-            tampilkanPesanan(i);
-        }
-    }
-      
-  public static  void tampilkanPesanan(){
-    for (int i = 0; i < pesanan.dataPesanan.size(); i++) {
-      System.out.println("nama: "+pesanan.dataPesanan.get(i));
-      System.out.println("jumlah: "+pesanan.dataTotalLembar.get(i));
-      System.out.println("harga1an: "+pesanan.dataHarga.get(i));
+  public static void TampilkanPesanan(int index) {
+    if (index < pesanan.dataPesanan.size()){
+      System.out.println("===PESANAN KE -" + (index + 1) + "===");
+      System.out.println("Nama :" + pesanan.dataPesanan.get(index));
+      System.out.println("Jumlah :" + pesanan.dataTotalLembar.get(index));
+      System.out.println("Harga per lembar : Rp" + pesanan.dataHarga.get(index));
+      System.out.println("-----------------------");
     }
   }
-
+  public static void TampilkanPesanan(){
+    if (pesanan.dataPesanan.isEmpty()) {
+      System.out.println("Belum ada data pesanan.");
+      return;
+    }
+    for(int i= 0;i < pesanan.dataPesanan.size(); i++){
+      TampilkanPesanan(i);
+    }
+  }
   public static void main(String[] args) {
     pesanan.dataPesanan.add("Pulpen");
     pesanan.dataHarga.add(5000);
     pesanan.dataTotalLembar.add(1);
-    pesanan.dataPesanan.add("pensil");
+
+    pesanan.dataPesanan.add("Pensil");
     pesanan.dataHarga.add(3500);
     pesanan.dataTotalLembar.add(1);
 
-
-    tampilkanPesanan();
+    TampilkanPesanan();
   }
-}
- }
+  }
+
